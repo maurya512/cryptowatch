81275098b2e88655292f0068616002c2ffc04a79
maurya512 <patelmaurya0512@gmail.com>
2021-03-11 23:12:07 -0500

    created and cleaned up the react app:

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# cryptowatch
+A basic app that lets you see the current price of majority cryptocurrencies in the world!
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        CRYPTO WATCH
+      </h1>
+    </div>
+  );
+}
+
+export default App;
