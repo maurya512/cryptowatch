258abd3656f3bd9dd29ebdac8ec5179a5909b80b
maurya512 <patelmaurya0512@gmail.com>
2021-03-15 23:39:11 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+// import { Router } from 'react-router';
+// import { Link } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import Coin from './Coin';
@@ -31,28 +33,29 @@ function App() {
   const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase())
   )
   return (
-    <div className="coin-app">
-      <div className='coin-search'>
-        <h1 className='coin-text'>Search a Currency</h1>
-        <form>
-          <input type='text' placeholder='Search' className='coin-input' onChange={handleChange} />
-        </form>
-      </div>
+      <div className="coin-app">
+        <div className='coin-search'>
+          <h1 className='coin-text'>Search a Currency</h1>
+          <form>
+            <input type='text' placeholder='Search' className='coin-input' onChange={handleChange} />
+          </form>
+        </div>
 
-      {/* map through the coins and display all of the results */}
-      {filteredCoins.map(coin => {
-        return (
-          <Coin key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            volume={coin.market_cap}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        )
-      })}
-    </div>
+        {/* map through the coins and display all of the results */}
+        {filteredCoins.map(coin => {
+          return (
+            <Coin key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+            />
+          )
+        })}
+      </div>
   );
 }
 
